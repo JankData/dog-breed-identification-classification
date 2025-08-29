4e800b3b43b94ae14329e6282b61ae16db917b49
JankData <karol.jank1@gmail.com>
2025-08-29 20:57:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,52 @@
 import gradio as gr
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+import tensorflow_hub as hub
 import numpy as np
 from PIL import Image
 
-MODEL_PATH = "194834-27082025-full-resnetv2-Adam.keras"
+WEIGHTS_PATH='models/resnetv2_classifier.weights.h5'
 
-CLASSES = [
-'affenpinscher','afghan_hound','african_hunting_dog','airedale','american_staffordshire_terrier',
-'appenzeller','australian_terrier','basenji','basset','beagle',
-'bedlington_terrier','bernese_mountain_dog','black-and-tan_coonhound','blenheim_spaniel','bloodhound',
-'bluetick','border_collie','border_terrier','borzoi','boston_bull',
-'bouvier_des_flandres','boxer','brabancon_griffon','briard','brittany_spaniel',
-'bull_mastiff','cairn','cardigan','chesapeake_bay_retriever','chihuahua',
-'chow','clumber','cocker_spaniel','collie','curly-coated_retriever',
-'dandie_dinmont','dhole','dingo','doberman','english_foxhound',
-'english_setter','english_springer','entlebucher','eskimo_dog','flat-coated_retriever',
-'french_bulldog','german_shepherd','german_short-haired_pointer','giant_schnauzer','golden_retriever',
-'gordon_setter','great_dane','great_pyrenees','greater_swiss_mountain_dog','groenendael',
-'ibizan_hound','irish_setter','irish_terrier','irish_water_spaniel','irish_wolfhound',
-'italian_greyhound','japanese_spaniel','keeshond','kelpie','kerry_blue_terrier',
-'komondor','kuvasz','labrador_retriever','lakeland_terrier','leonberg',
-'lhasa','malamute','malinois','maltese_dog','mexican_hairless',
-'miniature_pinscher','miniature_poodle','miniature_schnauzer','newfoundland','norfolk_terrier',
-'norwegian_elkhound','norwich_terrier','old_english_sheepdog','otterhound','papillon',
-'pekinese','pembroke','pomeranian','pug','redbone',
-'rhodesian_ridgeback','rottweiler','saint_bernard','saluki','samoyed',
-'schipperke','scotch_terrier','scottish_deerhound','sealyham_terrier','shetland_sheepdog',
-'shih-tzu','siberian_husky','silky_terrier','soft-coated_wheaten_terrier','staffordshire_bullterrier',
-'standard_poodle','standard_schnauzer','sussex_spaniel','tibetan_mastiff','tibetan_terrier',
-'toy_poodle','toy_terrier','vizsla','walker_hound','weimaraner',
-'welsh_springer_spaniel','west_highland_white_terrier','whippet','wire-haired_fox_terrier','yorkshire_terrier'
+NUM_CLASSES=120
+
+CLASSES=[
+    'affenpinscher','afghan_hound','african_hunting_dog','airedale','american_staffordshire_terrier',
+    'appenzeller','australian_terrier','basenji','basset','beagle',
+    'bedlington_terrier','bernese_mountain_dog','black-and-tan_coonhound','blenheim_spaniel','bloodhound',
+    'bluetick','border_collie','border_terrier','borzoi','boston_bull',
+    'bouvier_des_flandres','boxer','brabancon_griffon','briard','brittany_spaniel',
+    'bull_mastiff','cairn','cardigan','chesapeake_bay_retriever','chihuahua',
+    'chow','clumber','cocker_spaniel','collie','curly-coated_retriever',
+    'dandie_dinmont','dhole','dingo','doberman','english_foxhound',
+    'english_setter','english_springer','entlebucher','eskimo_dog','flat-coated_retriever',
+    'french_bulldog','german_shepherd','german_short-haired_pointer','giant_schnauzer','golden_retriever',
+    'gordon_setter','great_dane','great_pyrenees','greater_swiss_mountain_dog','groenendael',
+    'ibizan_hound','irish_setter','irish_terrier','irish_water_spaniel','irish_wolfhound',
+    'italian_greyhound','japanese_spaniel','keeshond','kelpie','kerry_blue_terrier',
+    'komondor','kuvasz','labrador_retriever','lakeland_terrier','leonberg',
+    'lhasa','malamute','malinois','maltese_dog','mexican_hairless',
+    'miniature_pinscher','miniature_poodle','miniature_schnauzer','newfoundland','norfolk_terrier',
+    'norwegian_elkhound','norwich_terrier','old_english_sheepdog','otterhound','papillon',
+    'pekinese','pembroke','pomeranian','pug','redbone',
+    'rhodesian_ridgeback','rottweiler','saint_bernard','saluki','samoyed',
+    'schipperke','scotch_terrier','scottish_deerhound','sealyham_terrier','shetland_sheepdog',
+    'shih-tzu','siberian_husky','silky_terrier','soft-coated_wheaten_terrier','staffordshire_bullterrier',
+    'standard_poodle','standard_schnauzer','sussex_spaniel','tibetan_mastiff','tibetan_terrier',
+    'toy_poodle','toy_terrier','vizsla','walker_hound','weimaraner',
+    'welsh_springer_spaniel','west_highland_white_terrier','whippet','wire-haired_fox_terrier','yorkshire_terrier'
 ]
 
-model = load_model(MODEL_PATH)
+def build_model():
+    model = tf.keras.Sequential([
+        hub.KerasLayer(
+            "https://tfhub.dev/google/imagenet/resnet_v2_50/feature_vector/5",
+            trainable=False
+        ),
+        tf.keras.layers.Dense(NUM_CLASSES, activation="softmax")
+    ])
+    model.load_weights(WEIGHTS_PATH)
+    return model
+
+model = build_model()
 
 def predict_breed(img: Image.Image):
     img = img.resize((224, 224))
@@ -41,7 +54,6 @@ def predict_breed(img: Image.Image):
     x = np.expand_dims(x, axis=0)
 
     preds = model.predict(x)[0]
-
     top3_idx = preds.argsort()[-3:][::-1]
     results = {CLASSES[i]: float(preds[i])*100 for i in top3_idx}
     return results
